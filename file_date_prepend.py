0db3286abd21951997474be3ebc1046c439fdb58
Shaun Ho <shaunhohohoho@gmail.com>
2018-10-31 20:06:33 +0000

    Completed, no tests written, but tool is able to make a copy of all the selected files and place them in a subfolder "updated_files". Tool recognises if file has been dated and ignores file if so. This only works when all of the files are on a single directory

--- file_date_prepend.py
+++ file_date_prepend.py
@@ -1,7 +1,13 @@
 import os
-from shutil import copyfile
+from shutil import copy2
 from datetime import datetime
 from dateutil import parser
+from collections import Counter
+import pyperclip
+import objc
+import sys
+import time
+import unicodedata
 
 
 def check_if_dated(file):
@@ -18,33 +24,74 @@ def add_date(root, file):
     mod_time = os.path.getmtime(filepath)
     mod_time = datetime.fromtimestamp(mod_time)
     mod_time = mod_time.strftime("%Y%m%d")
-    new_filepath = os.path.join(root,mod_time+file)
+    updated_files_directory = os.path.join(root,"updated_files")
+    if not os.path.exists(updated_files_directory):
+        os.makedirs(updated_files_directory)
+    new_filepath = os.path.join(root,"updated_files",mod_time+file)
+    copy2(filepath,new_filepath)
+    print new_filepath+" created!"
 
-    os.rename(filepath,new_filepath)
 
 def rename_files_in_folder(folderpath):
     for root, dirs, files in os.walk(folderpath):
         for file in files:
-            if not file.startswith("."):
-                if not check_if_dated(file):
-                    add_date(root,file)
+            if not file.startswith(".") and not check_if_dated(file):
+                add_date(root,file)
 
 
             
+def get_clipboard_filepaths():
+    clipboard_paths = repr(pyperclip.paste())
+    clipboard_paths = clipboard_paths.replace("u'","").replace("'","")
+    clipboard_paths_list = clipboard_paths.split('\\n')
+    basenames_list = [os.path.basename(clipboard_path) for clipboard_path in clipboard_paths_list]
+    dirnames_list = [os.path.dirname(clipboard_path) for clipboard_path in clipboard_paths_list]
+    return dirnames_list[0], basenames_list
+
+def rename_files_from_clipboard():
+    dirname, basenames_list = get_clipboard_filepaths()
+    if len(basenames_list)==0:
+        print 'Tool only works in a single directory and uses pathnames, use Alt Key after right clicking selection and choose "Copy "X" as pathname"'
+    elif dirname=="":
+        print 'Tool only works in a single directory and uses pathnames, use Alt Key after right clicking selection and choose "Copy "X" as pathname"'
+    elif len(basenames_list)==1 and os.path.isdir(os.path.join(dirname,basenames_list[0])):
+        folderpath = os.path.join(dirname,basenames_list[0])
+        rename_files_in_folder(folderpath)
+    else:
+        rename_files_in_selection(dirname, basenames_list)
+
+
+def rename_files_in_selection(root, files):
+    for file in files:
+        if not file.startswith(".") and not check_if_dated(file):
+            add_date(root,file)
+    
 
 
 
 
-folderpath="/Users/shaun/Documents/Python/test_folder"
+folderpath="/Users/shaun/Documents/Python/Tools/test_folder"
 test=False
 
 
 if __name__=="__main__":
+
+
     if test:
+        for root, dirs, files in os.walk(folderpath):
+            for file in files:
+                os.unlink(os.path.join(root,file))
         for filename in range(10):
             with open(os.path.join(folderpath,str(filename)+".txt"), "w") as f:
                 f.write("test")
     else:
-        rename_files_in_folder(folderpath)
-    
+
+        recent_value = pyperclip.paste()
+        while True:
+            tmp_value = pyperclip.paste()
+            if tmp_value != recent_value:
+                recent_value = tmp_value
+                rename_files_from_clipboard()
+            time.sleep(0.1)
+        
     
